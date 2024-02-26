21289ecf8777cad16624a8d61c309c03fbc5b8b9
Depyter <acabalharleyvan@gmail.com>
2024-02-26 22:26:26 +0800

    added random stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -19,7 +19,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.blue[200],
+        appBar: AppBar(
+          title: Text("Demo app tutorial"),
+          backgroundColor: Colors.blueAccent,
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.logout))
+          ],
+        ),
+        body: ListView(
+          children: [
+            // 1st child
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.lightBlue,
+            ),
+            // 2nd child
+            Container(
+              height: 250,
+              width: 250,
+              color: Colors.blueAccent,
+            ),
+            //3rd child
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.lightBlue,
+            ),
+          ],)
       ),
     );
   }
