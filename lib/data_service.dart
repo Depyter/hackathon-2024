f714e9ae5a8cf4857c95a18aba30360d5462d4b8
Depyter <acabalharleyvan@gmail.com>
2024-02-28 03:39:33 +0800

    radom commit

--- lib/data_service.dart
+++ lib/data_service.dart
@@ -5,7 +5,6 @@ final FirebaseFirestore firestore = FirebaseFirestore.instance;
 final CollectionReference users = firestore.collection('users');
 
 Future<void> addanswer(String answer) {
-  // Call the user's CollectionReference to add a new user
   return users
     .add({
       'answer': answer, // John Doe
