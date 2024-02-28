4170740f3a8416847631c7440942a2d860cc9ce3
tooliltoolate <jtibay21@gmail.com>
2024-02-28 12:10:09 +0800

    Your

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:hackathon_2024/user_question.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hackathon_2024/sign_up.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  runApp(SignUp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +16,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: UserInput(),
       ),
     );
   }
